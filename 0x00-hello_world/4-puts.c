d0cf6118d316ac53db13b2a7e6531c45da585cd8
Josephine mukulu <jmngina39@gmail.com>
2023-03-16 16:32:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
- * Return: Always 0.
+ * Return; Always 0.
  */
 
 int main(void)
@@ -10,5 +10,5 @@ int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
 
-        return (0):
+        return (0);
 }
