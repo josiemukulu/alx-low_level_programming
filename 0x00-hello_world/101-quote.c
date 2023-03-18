9eb19ea5d4856ce72bb30a15443a1c81b7223d25
Josephine mukulu <jmngina39@gmail.com>
2023-03-18 08:15:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main a c program
+ * main - a c program
  * return 1 (success)
  */
 int main(void)
 {
+
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
-}	
+return (1);
+}
