ef6ef1c164e5536e7bd881ffb8d09ebcae5af8c6
Josephine mukulu <jmngina39@gmail.com>
2023-03-18 09:27:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,12 @@
 /**
  * main - A c program that prints a line to the standard without error
  * return 1 (success)
+ *
  */
 int main(void)
 {
 
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
+
 }
