91c27ed4618b2151bdc98377ab75e64382ddcde6
Josephine mukulu <jmngina39@gmail.com>
2023-03-19 12:09:31 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,25 +7,25 @@
  */
 int main(void)
 {
-        int i;
-        int j;
+int i;
+int j;
 
-        for (i = 0 ; i < 10 ; i++)
-        {
-                for (j = 1 ; j < 10 ; j++)
-                {
-                        if (i < j && i != j)
-                        {
-                                putchar(i + '0');
-                                putchar(j + '0');
-                                if (i + j != 17)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = 1 ; j < 10 ; j++)
+{
+if (i < j && i != j)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i + j != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
