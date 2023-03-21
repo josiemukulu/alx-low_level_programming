bc3258db998fcde6459256236bea4384c48fc80a
Josephine mukulu <jmngina39@gmail.com>
2023-03-21 14:38:48 -0700

    ./1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+void print_alphabet(void)
+
+{
+
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
+}
