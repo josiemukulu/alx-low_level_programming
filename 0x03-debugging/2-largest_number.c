cb967741096cd7b6d52ea8b7460047815b468dc8
Josephine mukulu <jmngina39@gmail.com>
2023-03-23 10:28:30 -0700

    2-largest_number.c, main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,21 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a > b && b > c)
 {
 largest = a;
 }
-else if (a > b && c > a)
-{
+else if (a > c && c > b)
+largest = a;
+else if (b > a && a > c)
+largest = b;
+else if (b > c && c > a)
+largest = b;
+else if (c > a && a > b)
 largest = c;
-}
-else if (b > c)
-{
-largest = b
-}
 else
 {
-largest = a;
+largest = c;
 }
 
 return (largest);
